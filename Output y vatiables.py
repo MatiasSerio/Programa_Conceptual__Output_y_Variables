535db3c3b6a75f4da8696667c50ae64376fd3571
MatiasSerio <105302077+MatiasSerio@users.noreply.github.com>
2022-05-11 12:03:32 -0300

    Cree programa.py

---
+++ Output y vatiables.py
@@ -0,0 +1,13 @@
+print("Output y variables") 
+
+# La primera se debe llamar nombre y debe contener tu nombre
+# (entre comillas).
+# La segunda se debe llamar edad y debe contener tu edad 
+# (como un número simple, sin comillas).
+# Por último el programa debe mostrar en pantalla la siguiente frase en una sóla línea:
+#                          (nombre) tiene (edad) años
+
+nombre = "Matías"
+edad = 23 
+
+print(nombre, "tiene", edad, "años.")
